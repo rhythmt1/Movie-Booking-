a5cfe3ddd97b58ce15be8bf11427abd50e2c277e
rhythmtrivedi <rhythmt1@students.cs.ubc.ca>
2022-03-04 00:58:09 -0800

    Fixed problems from phase 1.

--- src/main/ui/MovieBookingApp.java
+++ src/main/ui/MovieBookingApp.java
@@ -20,18 +20,50 @@ public class MovieBookingApp {
         tickets = new MyTickets();
 
         input = new Scanner(System.in);
-        chooseMovie();
+        homePage();
         command = input.next();
-        processCommand(command);
+        processHome(command);
         ticket = new Ticket(movie);
-        selectSeat(command);
-        addTickets(command);
-        payment();
-        System.out.println(tickets.getNumOfTickets() + " tickets have been purchased");
-        System.out.println("Thank you and enjoy your movie!");
-        rate();
         System.out.println("Thank you and enjoy your day!");
+        homePage();
+        command = input.next();
+        processHome(command);
 
+
+    }
+
+    private void homePage() {
+        System.out.println("\nSelect from:");
+        System.out.println("\t1 - Choose a movie");
+        System.out.println("\t2 - Add more tickets");
+        System.out.println("\t3 - Rate Movie");
+        System.out.println("\t4 - See Movie Ratings");
+        System.out.println("\t5 - View Payment");
+        System.out.println("\t6 - Exit");
+
+
+
+
+
+    }
+
+    private void processHome(String command) {
+        if (command.equals("1")) {
+            watch(command);
+        } else if (command.equals("2")) {
+            addTickets(command);
+        } else if (command.equals("3")) {
+            rate();
+        } else if (command.equals("4")) {
+            movie.getRating();
+        } else if (command.equals("6")) {
+            System.out.println("Goodbye");
+        } else if (command.equals("5")) {
+            payment();
+        } else {
+            System.out.println("Invalid entry");
+            homePage();
+        }
 
     }
 
@@ -41,6 +73,8 @@ public class MovieBookingApp {
         System.out.println("\t2 - The Batman at 9:30pm");
         System.out.println("\t3 - Spider-man at 7:00pm");
         System.out.println("\t4 - Uncharted at 10:45pm");
+
+
 
     }
 
@@ -57,6 +91,7 @@ public class MovieBookingApp {
             System.out.println("Invalid entry");
             runBooking();
         }
+        selectSeat(command);
 
     }
 
@@ -99,15 +134,21 @@ public class MovieBookingApp {
         command = input.next();
 
         if (command.equals("yes")) {
-            runBooking();
-            tickets.addTicket(ticket);
+            watch(command);
+
 
 
         } else if (command.equals("no")) {
             System.out.println("Have a nice day!");
         } else {
             System.out.println("Invalid entry");
+
         }
+        System.out.println(tickets.getNumOfTickets() + " tickets have been purchased");
+        homePage();
+        command = input.next();
+        processHome(command);
+
     }
 
     private void selectSeat(String command) {
@@ -115,25 +156,29 @@ public class MovieBookingApp {
         command = input.next();
 
         System.out.println("seat " + command + " as been booked for you");
+        payment();
     }
 
     private void payment() {
 
         System.out.println("Your payment is $" + tickets.getTotalPrice());
+
     }
 
     private void rate() {
 
-        System.out.println("Please rate the movie");
+        System.out.println("Please rate the movie from 1-10");
 
         double command = input.nextDouble();
         movie.giveRating(command);
+        movie.getRating();
     }
 
-
-
-
-
+    private void watch(String command) {
+        chooseMovie();
+        command = input.next();
+        processCommand(command);
+    }
 
 
 
